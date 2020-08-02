c409fc5ecdf2d759f0f2456e600a7cc2e70a44e0
menglei0602 <menglei0602@aliyun.com>
2020-08-02 23:44:36 +0800

    eslint中设置在函数后可以不加空格

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "vue/no-parsing-error": [2, { "x-invalid-end-tag": false }],
     quotes: [2, "double"],
-    semi: [2, "always"]
+    semi: [2, "always"],
+    'space-before-function-paren': 0
   }
 };
